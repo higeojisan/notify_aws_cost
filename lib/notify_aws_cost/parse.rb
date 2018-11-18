19488543a4cbb9bf9a154b1eda49be2114c14244
higeojisan <makotokoda82@gmail.com>
2018-11-18 15:42:10 +0900

    Display total charge

--- lib/notify_aws_cost/parse.rb
+++ lib/notify_aws_cost/parse.rb
@@ -6,13 +6,17 @@ module NotifyAwsCost
 
     def each_service(each_service_hash)
       result = ""
+      sum = 0
       each_service_hash.each do |key, val|
         if val.datapoints == []
           result += "#{key}: N/A\n\n"
         else
-          result += "#{key}: $#{val.datapoints[0].maximum}USD\n\n"
+          result += "#{key}: $#{val.datapoints[0].maximum}\n\n"
+          sum += val.datapoints[0].maximum
         end
       end
+      result += "============================\n\n"
+      result += "Total: $#{sum}"
       result
     end
 
