43818827e0a479cf1c34c5fc6cfe122920f09222
higeojisan <makotokoda82@gmail.com>
2018-11-20 21:37:05 +0900

    Added one test for NotifyAwsCost::AwsCost

--- spec/notify_aws_cost_spec.rb
+++ spec/notify_aws_cost_spec.rb
@@ -3,7 +3,41 @@ RSpec.describe NotifyAwsCost do
     expect(NotifyAwsCost::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  #it "does something useful" do
+  #  expect(false).to eq(true)
+  #end
 end
+
+ RSpec.describe NotifyAwsCost::AwsCost do
+
+
+  context "get_service_list" do
+    it "normal" do
+      correct_hash = {
+        metrics: [
+          {
+            namespace: "AWS/Billing",
+            metric_name: "EstimatedCharges",
+            dimensions: [
+              {
+                name: "ServiceName",
+                value: "AmazonSNS",
+              },
+              {
+                name: "Currency",
+                value: "USD",
+              },
+            ]
+          }
+        ]
+      }
+      Aws.config[:cloudwatch] = {
+        stub_responses: {
+          list_metrics: correct_hash
+        }
+      }
+      aws_cost = NotifyAwsCost::AwsCost.new
+      expect(aws_cost.send(:get_service_list).to_h).to eq correct_hash
+    end
+  end
+ end
